f26fd120ddc0cbd11f2f9ec85242829ab0cbc2b8
Akanksha Kashyap <akashyapdeep@gmail.com>
2024-03-12 12:04:58 +0530

    rename

--- 1_cv.py
+++ 1_cv.py
@@ -8,8 +8,8 @@ image = np.zeros((500, 500, 3), dtype="uint8")
 
 cv2.rectangle(image, (50, 50), (200, 150), (255, 0, 0), -1)
 cv2.circle(image, (300, 100), 50, (0, 255, 0), -1)
-cv2.line(image, (100, 300), (300, 300), (0, 0, 255), 1)
+cv2.line(image, (100, 300), (300, 300), (0, 0, 255), 2)
 
-cv2.imshow("2D objects image",image)
+cv2.imshow("2D Objects Image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
