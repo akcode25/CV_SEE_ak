f5f634b1f78df858985f67a26dfb6cc3484de48f
Akanksha Kashyap <akashyapdeep@gmail.com>
2024-03-13 21:38:05 +0530

    9 and 10

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CV_SEE_ak
+git clone https://github.com/akcode25/CV_ak.git<br>
+git clone https://github.com/virat121/CV.git
+https://colab.research.google.com/drive/1nZvXsNbu1etnFTzfbQ6JNGOJkj_BfpPr?usp=sharing<br>
+pip install opencv-python<br>
+mub.me/all8<br>
--- project_10.py
+++ project_10.py
@@ -1,6 +1,5 @@
 import cv2
 
-# Path to your image file
 image_path = "C:/Users/aka-pc/Downloads/hp.jpg"
 image = cv2.imread(image_path) # Load the image
 
@@ -12,19 +11,18 @@ else:
     # This line creates a cascade classifier object for detecting faces using a pre-trained Haar cascade classifier. The path to the Haar cascade file for face detection is provided.
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')  #classifier object
 
-    # Convert the image to grayscale for face detection - simplify processing and reduce computational complexity.
+    # Convert to grayscale for face detection - simplify processing and reduce computational complexity.
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the image
     faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.3, minNeighbors=5) # returns a list of rectangles representing the detected faces.  #a larger value of scaleFactor speeds up the process but may miss smaller faces # each potential face rectangle should have at least 5 neighboring rectangles to be considered a valid face.
 
-    # Check if faces are detected
     if len(faces) > 0:
         print("Human face detected!")
     else:
         print("No human face detected.")
 
-    # Draw green rectangles around the detected faces
+    # green rectangles around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 5)
     # Resize the image for better visibility
@@ -33,4 +31,4 @@ else:
     # Display the original image with faces highlighted
     cv2.imshow('Detected Faces', resized_image)
     cv2.waitKey(0)  # program will wait indefinitely until a key is pressed
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows() # release all resources
